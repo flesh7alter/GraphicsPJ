41ee13abeff21253a4d7b89c344160de4ae2be7c
unknown <16307130321@fudan.edu.cn>
2019-12-09 17:40:20 +0800

    modified:   PJ2/main.c

--- PJ2/main.c
+++ PJ2/main.c
@@ -9,14 +9,13 @@ GLfloat white_light[] = { 1.0, 1.0, 1.0, 1.0 };
 GLfloat Light_Model_Ambient[] = { 0.2, 0.2, 0.2, 1.0 };
 
 void init() {
-	glClearColor(1.0,1.0,1.0,1.0);
-	glShadeModel(GL_SMOOTH);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, Light_Model_Ambient);  
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, Light_Model_Ambient);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
@@ -38,7 +37,6 @@ void reshape(int w, int h)
 		glOrtho(-1.5, 1.5, -1.5*(GLfloat)h / (GLfloat)w, 1.5*(GLfloat)h / (GLfloat)w, -10.0, 10.0);
 	else
 		glOrtho(-1.5*(GLfloat)w / (GLfloat)h, 1.5*(GLfloat)w / (GLfloat)h, -1.5, 1.5, -10.0, 10.0);
-
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
